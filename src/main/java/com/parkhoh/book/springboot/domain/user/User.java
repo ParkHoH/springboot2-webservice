3c12755a5fb7d36fd9c054bea7b96477bdd038c6
Hohyun Park <ioio6382@gmail.com>
2022-04-08 23:39:59 +0900

    Feat: OAuth 추가

---
+++ src/main/java/com/parkhoh/book/springboot/domain/user/User.java
@@ -0,0 +1,50 @@
+package com.parkhoh.book.springboot.domain.user;
+
+
+import com.parkhoh.book.springboot.domain.BaseTimeEntity;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Getter
+@NoArgsConstructor
+@Entity
+public class User extends BaseTimeEntity {
+
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String email;
+
+    @Column
+    private String picture;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Role role;
+
+    @Builder
+    public User(String name, String email, String picture) {
+        this.name = name;
+        this.email = email;
+        this.picture = picture;
+    }
+
+    public User update(String name, String picture) {
+        this.name = name;
+        this.picture = picture;
+        return this;
+    }
+
+    public String getRoleKey() {
+        return this.role.getKey();
+    }
+
+}
